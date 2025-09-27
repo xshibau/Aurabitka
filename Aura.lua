16f45c804fe24bdaa76d7ebe09e3071726938f8e
xshibau <ziugpro@gmail.com>
2025-09-27 13:36:47 +0700

    Update Aura.lua

--- Aura.lua
+++ Aura.lua
@@ -3,7 +3,7 @@ local ScriptsPremium = {
  [7709344486] = "https://raw.githubusercontent.com/ziugpro/Tool-Hub/refs/heads/main/QuanlySaB.lua",
  [7851663243] = "https://raw.githubusercontent.com/ziugpro/Tool-Hub/refs/heads/main/QuanlyStealaMeme.lua",
  [7750955984] = "https://raw.githubusercontent.com/ziugpro/Tool-Hub/refs/heads/main/QuanlyHuntyZombie.lua",
- [7018190066] = "https://raw.githubusercontent.com/ziugpro/Tool-Hub/refs/heads/main/Quanly-DeadRails.lua",
+ [7018190066] = "https://raw.githubusercontent.com/xshibau/Aurabitka/refs/heads/main/Quanly-DeadRails.lua",
  [7939389895] = "https://raw.githubusercontent.com/xshibau/Aurabitka/refs/heads/main/QuanlyMadRoad.lua",
         }
         local url = ScriptsPremium[game.PlaceId] or ScriptsPremium[game.GameId]
