c11474ec9ac8efb52dbe35efa0a27c9db32187af
xshibau <ziugpro@gmail.com>
2025-10-13 20:30:31 +0700

    Update Aura.lua

--- Aura.lua
+++ Aura.lua
@@ -1,9 +1,9 @@
 if game.GameId == 7326934954 then
- loadstring(game:HttpGet("https://raw.githubusercontent.com/xshibau/Aurabitka/refs/heads/main/Quanly99Night.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/xshibau/Aurabitka/refs/heads/main/Quanly99Night.lua"))()
 elseif game.PlaceId == 7449423635 or game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 122478697296975 then
- loadstring(game:HttpGet("https://raw.githubusercontent.com/xshibau/Aurabitka/refs/heads/main/QuanlyBloxKid.luau"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/xshibau/Aurabitka/refs/heads/main/QuanlyBloxKidEng.lua"))()
 elseif game.GameId == 8066283370 then
- loadstring(game:HttpGet("https://raw.githubusercontent.com/xshibau/Aurabitka/refs/heads/main/QuanlyBulAZoo.luau"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/xshibau/Aurabitka/refs/heads/main/QuanlyBulAZoo.luau"))()
 elseif game.GameId == 7709344486 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/ziugpro/Tool-Hub/refs/heads/main/QuanlySaB.lua"))()
 elseif game.GameId == 7750955984 then
